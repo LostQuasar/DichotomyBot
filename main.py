c5d31a8dc90823593f021d7508fa578617d717cd
LostQuasar <rionlion100@gmail.com>
2024-05-12 13:42:31 -0400

    Extract secrets

--- main.py
+++ main.py
@@ -11,33 +11,34 @@ load_dotenv()
 name = "Dichotomy"
 bot = discord.Bot()
 token = os.getenv("TOKEN")
+channel_id = os.getenv("CHANNEL_ID")
+sub_id = os.getenv("SUB_ID")
 intents = discord.Intents.default()
 intents.message_content = True
-botClient = discord.Client(intents=intents)
-vibeDevice: buttplug.Device
-vibeClient: buttplug.Client
+bot_client = discord.Client(intents=intents)
+vibe_device: buttplug.Device
+vibe_client: buttplug.Client
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-async def updateStatus(num):
-    await botClient.change_presence(
+async def update_status(num):
+    await bot_client.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.playing, name=f"with {num}/2"
         )
     )
 
 
-@botClient.event
+@bot_client.event
 async def on_message(message):
-    global vibeClient
+    global vibe_client
     strength = 1
 
     if (
-        message.channel
-        != 1238171375634485311 | message.author
-        == botClient.user | message.author
-        == 208273752956338198
+        message.channel != channel_id | 
+        message.author == bot_client.user | 
+        message.author == sub_id
     ):
         return
 
@@ -52,50 +53,50 @@ async def on_message(message):
         await message.add_reaction("🔌")
         await punish_fox(strength)
     elif message.content == "$RetryConnect":
-        await vibeClient.disconnect()
+        await vibe_client.disconnect()
         await on_ready()
 
 
 async def reward_fox(strength):
-    global vibeDevice
-    await vibeDevice.actuators[0].command(strength / 10)
+    global vibe_device
+    await vibe_device.actuators[0].command(strength / 10)
     time.sleep(strength)
-    await vibeDevice.actuators[0].command(0)
+    await vibe_device.actuators[0].command(0)
 
 
 async def punish_fox(strength):
     None
 
 
-@botClient.event
+@bot_client.event
 async def on_ready():
-    global vibeDevice
-    global vibeClient
+    global vibe_device
+    global vibe_client
     connected = 0
-    vibeClient = buttplug.Client(name, buttplug.ProtocolSpec.v3)
+    vibe_client = buttplug.Client(name, buttplug.ProtocolSpec.v3)
     connector = buttplug.WebsocketConnector(
-        "ws://127.0.0.1:12345", logger=vibeClient.logger
+        "ws://127.0.0.1:12345", logger=vibe_client.logger
     )
 
-    await updateStatus(connected)
+    await update_status(connected)
     try:
-        await vibeClient.connect(connector)
-        if len(vibeClient.devices) == 0:
+        await vibe_client.connect(connector)
+        if len(vibe_client.devices) == 0:
             logging.error("No devices connected to Intifcae")
             return
-        vibeDevice = vibeClient.devices[0]
-        if len(vibeDevice.actuators) == 0:
+        vibe_device = vibe_client.devices[0]
+        if len(vibe_device.actuators) == 0:
             logging.error("Wrong Intiface device connected")
             return
         connected += 1
-        await updateStatus(connected)
+        await update_status(connected)
     except Exception as e:
         logging.error(f"Could not connect to server, exiting: {e}")
 
 
-@botClient.event
+@bot_client.event
 async def on_disconnect():
-    await vibeClient.disconnect()
+    await vibe_client.disconnect()
 
 
-botClient.run(token)
+bot_client.run(token)
