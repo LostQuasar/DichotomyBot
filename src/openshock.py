4d2b624e6f29ce1812657f74a668dcdc1bb5bcd4
LostQuasar <saiph.williams@gmail.com>
2024-06-06 15:09:00 -0400

    Keep track of children

--- src/openshock.py
+++ src/openshock.py
@@ -1,31 +1,27 @@
 from enum import Enum
 import requests
 
+
 class ControlType(Enum):
     VIBRATE = "Vibrate"
     SHOCK = "Shock"
     SOUND = "Sound"
 
-class openshock_api:
-    def __init__(self, api_key, url: str = "https://api.shocklink.net/"):
-        self.url = url
-        self.headers = {
-            "Content-type": "application/json",
-            "accept": "application/json",
-            "OpenShockToken": api_key,
-        }
 
-    def create_shocker(self, shocker_id):
-        return self.shocker(self, shocker_id)
+class openshock_api:
 
     class shocker:
-
         def __init__(self, parent, shocker_id: str):
             self.parent = parent
             self.shocker_id = shocker_id
 
         async def control(
-            self, type: ControlType, intensity: int, duration: int, author: str, exclusive:bool = True
+            self,
+            type: ControlType,
+            intensity: int,
+            duration: int,
+            author: str,
+            exclusive: bool = True,
         ):
             """Send a control signal to the shocker
 
@@ -54,3 +50,19 @@ class openshock_api:
                 },
                 headers=self.parent.headers,
             )
+
+    connected_shockers: list[shocker]
+
+    def __init__(self, api_key, url: str = "https://api.shocklink.net/"):
+        self.url = url
+        self.headers = {
+            "Content-type": "application/json",
+            "accept": "application/json",
+            "OpenShockToken": api_key,
+        }
+        self.connected_shockers = []
+
+    def create_shocker(self, shocker_id):
+        new_shocker = self.shocker(self, shocker_id)
+        self.connected_shockers.append(new_shocker)
+        return new_shocker
